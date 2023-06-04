d68f22dad57f471d9a019deb845243c1dae71773
Vladislav Skripniuk <skripniuk.v@yandex.ru>
2023-06-04 23:00:05 +0200

    homework 2

--- hw2/train.py
+++ hw2/train.py
@@ -1,10 +1,9 @@
-import argparse
 import os
 import pickle
+import click
 
 import mlflow
-
-mlflow.sklearn.autolog()
+mlflow.autolog()
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
@@ -15,27 +14,24 @@ def load_pickle(filename: str):
         return pickle.load(f_in)
 
 
-def run(data_path):
-
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
 
     with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
         rf = RandomForestRegressor(max_depth=10, random_state=0)
         rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_valid)
+        y_pred = rf.predict(X_val)
 
-        rmse = mean_squared_error(y_valid, y_pred, squared=False)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--data_path",
-        default="./output",
-        help="the location where the processed NYC taxi trip data was saved.",
-    )
-    args = parser.parse_args()
-
-    run(args.data_path)
+if __name__ == '__main__':
+    run_train()
